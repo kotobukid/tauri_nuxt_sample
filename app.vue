737eff118f286a2158fbd42b3d011ccbb4488d28
kotobukid <kotobuki@daqua.jp>
2024-06-21 15:22:28 +0900

    Tauri + Nuxt

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="less">
+span {
+  color: red;
+}
+span.orange {
+  color: orange;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="about">
+  <h1>About us</h1>
+  <p>Welcome to out website</p>
+  <nuxt-link to="/">HOME</nuxt-link>
+</div>
+</template>
+
+<script lang="ts">
+import { useHeadTitle } from '~/helpers/useHeadTitle'
+import { useHead } from '#imports'
+
+const headTitle = useHeadTitle('About')
+useHead(headTitle);
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped lang="less">
+h1 {
+  font-size: 1.5rem;
+}
+
+p {
+  color: grey;
+}
+</style>
