61b6104794fb377984d8c78aae0c305d9cc043a5
kotobukid <kotobuki@daqua.jp>
2024-06-21 15:43:53 +0900

    AppConfigをコンポーネント内で呼び出すことは問題がない pagex/index.vue表示中に再読込しても問題なし

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {useCount} from "../composables/count";
+import {useAppConfig} from "nuxt/app";
 
 const {
   count,
@@ -7,15 +8,21 @@ const {
   incrBy
 } = useCount();
 
+const appConfig = useAppConfig();
+const log_appConfig = () => {
+  console.log(appConfig);
+};
 
 </script>
 
 <template>
   <span class="count">{{ count }}</span>
   <br/>
-  <button @click="increment"> + 1 </button>
+  <button @click="increment"> + 1</button>
   <br/>
-  <button @click="incrBy(2)"> + 2 </button>
+  <button @click="incrBy(2)"> + 2</button>
+  <br/>
+  <button @click="log_appConfig">??</button>
 </template>
 
 <style scoped lang="less">
