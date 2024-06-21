d89bdc172ef91d63f67708c5d8677fdf4943a730
kotobukid <kotobuki@daqua.jp>
2024-06-21 17:01:12 +0900

    vite用設定はNuxtでは無視されるのでnuxt.config.tsに転記しろとの警告に従う

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  // plugins: [
-  //     '~/plugins/pinia.ts'
-  // ]
-    modules: ['@pinia/nuxt']
+    devtools: {enabled: true},
+    // plugins: [
+    //     '~/plugins/pinia.ts'
+    // ]
+    modules: ['@pinia/nuxt'],
+    // plugins: [vue()],
+    vite: {
+        clearScreen: false,
+        server: {
+            port: 1420,
+            strictPort: true,
+            watch: {
+                // 3. tell vite to ignore watching `src-tauri`
+                ignored: ["**/src-tauri/**"],
+            },
+        }
+    }
 })
