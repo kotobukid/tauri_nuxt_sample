a15f7301b01e344e9413ae6e0f8b261434a6403f
kotobukid <kotobuki@daqua.jp>
2024-06-21 16:52:07 +0900

    PiniaについてはNuxt用に定義済みのものが存在したらしい

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  plugins: [
-      '~/plugins/pinia.ts'
-  ]
+  // plugins: [
+  //     '~/plugins/pinia.ts'
+  // ]
+    modules: ['@pinia/nuxt']
 })
--- plugins/pinia.ts
+++
@@ -1,8 +0,0 @@
-// import { defineNuxtPlugin } from 'nuxt3'
-import {createPinia, setActivePinia} from 'pinia'
-
-export default defineNuxtPlugin(nuxt => {
-  const pinia = createPinia()
-  setActivePinia(pinia)
-  nuxt.provide('pinia', pinia)
-})
