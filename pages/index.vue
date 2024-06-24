5eef281ed31681530fef9c44ed49f3c95f304293
kotobukid <kotobuki@daqua.jp>
2024-06-24 20:08:00 +0900

    パラメータ込みで動的にルーティングをする
    旧版Nuxtの記事が当たり前に出てきて戸惑った

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
 <div>HOME</div>
   <ul>
     <li><nuxt-link to="about">About!</nuxt-link></li>
+    <li v-for="user in users" :key="user.id">
+      <nuxt-link :to="`/user/${user.id}_detail`">USER id {{ user.id }} {{ user.name }} /user/{{ user.id }}</nuxt-link>
+    </li>
   </ul>
   <Counter></Counter>
 </template>
@@ -18,6 +21,14 @@ useHead(headTitle);
 // export default {
 //   name: 'indexPage'
 // }
+
+const users = [
+  {id: 1, name: 'taro'},
+  {id: 2, name: 'jiro'},
+  {id: 3, name: 'saburo'},
+  {id: 4, name: 'hanako'},
+  {id: 5, name: 'jane'},
+];
 </script>
 
 <style scoped lang="less">
---
+++ pages/user/[id]_[method].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useRoute} from "nuxt/app";
+import {onMounted} from "vue";
+
+const route = useRoute();
+
+const userId = ref(null);
+const method = ref('');
+
+onMounted(() => {
+  userId.value = route.params.id;
+  method.value = route.params.method;
+});
+
+
+</script>
+
+<template>
+  <nuxt-link to="/">HOME</nuxt-link>
+  <br>
+  <h1>Detail page({{ method }})</h1>
+  <span>ID: {{ userId }}</span>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
